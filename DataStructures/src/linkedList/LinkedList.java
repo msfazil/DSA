89c165de866d6f607614fe55462bbf74b2b434a5
fazil <fazil424@gmail.com>
2023-09-18 21:52:49 +0530

    add construct and print method

--- DataStructures/src/linkedList/LinkedList.java
+++ DataStructures/src/linkedList/LinkedList.java
@@ -2,4 +2,34 @@ package linkedList;
 
 public class LinkedList {
 
+	private Node head;
+	private Node tail;
+	private int length;
+
+	class Node {
+
+		int value;
+		Node next;
+
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+
+	public void print() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+
+		}
+	}
+
 }
