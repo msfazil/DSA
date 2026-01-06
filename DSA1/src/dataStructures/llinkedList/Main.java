2a5bc8b4beefff9555bf91f97acc06d9b72e729c
fazil <fazil424@gmail.com>
2026-01-06 21:58:54 +0530

    edit

--- DSA1/src/dataStructures/llinkedList/Main.java
+++ DSA1/src/dataStructures/llinkedList/Main.java
@@ -4,16 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	LinkedList ll = new LinkedList(23);
-	ll.prepend(22);
-	ll.append(11);
-	ll.append(10);
-	System.out.print("before reverse: ");
-	ll.print();
-	ll.getLength();
-	ll.reverse();
-	System.out.print("after reverse: ");
-	ll.print();
+		LinkedList ll = new LinkedList(23);
+		ll.prepend(22);
+		ll.append(11);
+		ll.append(10);
+		System.out.print("before reverse: ");
+		ll.print();
+		ll.getLength();
+		ll.reverse();
+		System.out.println("");
+		System.out.print("after reverse: ");
+		ll.print();
 	}
 
 }
